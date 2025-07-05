6f5a52f9cb56444520e1f7e303ab5eab6d63cc12
DEV <pattnaikd833@gmail.com>
2025-07-05 20:28:42 +0530

    new routes introduce

--- froentend/src/main.jsx
+++ froentend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import ReactDOM from 'react-dom/client';
+// import { AuthProvider } from './context/AuthContext';
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './context/AuthContext';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+);
